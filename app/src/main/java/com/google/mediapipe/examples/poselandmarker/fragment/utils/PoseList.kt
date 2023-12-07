71c2c83b148015ace1966587c780cc89288e4ef3
drgrico <riccardodrago0918@gmail.com>
2023-12-07 17:33:10 +0900

    Item divider added
    - Item divider added (recycle view)

--- app/src/main/java/com/google/mediapipe/examples/poselandmarker/fragment/utils/PoseList.kt
+++ app/src/main/java/com/google/mediapipe/examples/poselandmarker/fragment/utils/PoseList.kt
@@ -1,10 +1,13 @@
 package com.google.mediapipe.examples.poselandmarker.fragment.utils
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.security.AccessController.getContext
 import com.google.mediapipe.examples.poselandmarker.R
 
 
@@ -17,6 +20,17 @@ class PoseList : AppCompatActivity() {
         setContentView(R.layout.activity_pose_list)
 
         recyclerView = findViewById(R.id.recycle_view)
+//        recyclerView.addItemDecoration(
+//            DividerItemDecoration(
+//                this,
+//                DividerItemDecoration.VERTICAL
+//            )
+//        )
+
+        val itemDecorator = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        itemDecorator.setDrawable(ContextCompat.getDrawable(this, R.drawable.item_divider)!!)
+        recyclerView.addItemDecoration(itemDecorator)
+
         poseAdapter = PoseAdapter(getPoseList())
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = poseAdapter
